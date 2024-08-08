2a397435a4dd67287d459d2245670f8c9ac136af
akweb69 <akwebdev69@gmail.com>
2024-08-08 18:39:48 +0600

    new design

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ window.onload = function() {
     var loader = document.getElementById('loader');
     loader.style.display = 'none';
 }
+
+
+
+function toggleMenu() {
+    
+    const menuIcon = document.getElementById("barIcon");
+    const crossIcon = document.getElementById("crossIcon");
+    const navLink = document.getElementById("nav-link-mobile");
+    
+
+    menuIcon.addEventListener('click' , function(){
+        navLink.style = " display: block";
+        crossIcon.style = "display: block" ; 
+        menuIcon.style =  "display : none" ; 
+       
+    }); 
+    crossIcon.addEventListener('click' , function(){
+        navLink.style = " display: none" ;
+        crossIcon.style = "display: none" ; 
+        menuIcon.style =  "display : block" ; 
+       
+    }); 
+}
+toggleMenu() ;
